fa2732317173e0ea07e4a418fdc6fc18ea8636da
Abhinav Tirath <tirathabhinav@gmail.com>
2018-01-25 22:19:24 -0500

    Completed the required Person5.java

--- src/main/java/edu/gatech/oad/antlab/person/Person5.java
+++ src/main/java/edu/gatech/oad/antlab/person/Person5.java
@@ -3,8 +3,8 @@ package edu.gatech.oad.antlab.person;
 /**
  *  A simple class for person 5
  *  returns their name and a
- *  modified string 
- *  
+ *  modified string
+ *
  *  @author Bob
  *  @version 1.1
  */
@@ -31,19 +31,28 @@ public class Person5 {
 	 */
 	private String calc(String input) {
 	  //Person 5 put your implementation here
-	  return null;
+	  String toBeMoved = input.substring(0, 2);
+	  String rest = input.substring(2, input.length());
+	  return (rest += toBeMoved);
 	}
-	
+
 	/**
 	 * Return a string rep of this object
 	 * that varies with an input string
 	 *
 	 * @param input the varying string
-	 * @return the string representing the 
+	 * @return the string representing the
 	 *         object
 	 */
 	public String toString(String input) {
 	  return name + calc(input);
+	}
+
+	public static void main(String[] args) {
+		Person5 five = new Person5("five");
+		System.out.println(five.calc("blah"));
+		System.out.println(five.toString("blah"));
+
 	}
 
 }
