87dfb26581e45ba433edf8a0a8e03c092c081655
Nitya Tarakad <nitarakad@gmail.com>
2018-01-25 22:27:18 -0500

    1st commit of Person1.java

--- src/main/java/edu/gatech/oad/antlab/person/Person1.java
+++ src/main/java/edu/gatech/oad/antlab/person/Person1.java
@@ -5,7 +5,7 @@ package edu.gatech.oad.antlab.person;
  *  returns their name and a
  *  modified string 
  *  
- *  @author Bob
+ *  @author Nitya Tarakad
  *  @version 1.1
  */
 public class Person1 {
@@ -30,8 +30,12 @@ public class Person1 {
 	 * @return the modified string
 	 */
 	private String calc(String input) {
-	  //Person 1 put your implementation here
-	  return null;
+	  if (input.length > 2) {
+	  	String firstTwo = input.substring(0, 2);
+	  	return input.substring(2) + firstTwo;
+	  }
+
+	  return input;
 	}
 	
 	/**
@@ -45,5 +49,4 @@ public class Person1 {
 	public String toString(String input) {
 	  return name + calc(input);
 	}
-
 }
