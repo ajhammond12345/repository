25dfe87d846a801bf9db10c66c910a5e3c3c9eb7
Nitya Tarakad <nitarakad@gmail.com>
2018-01-28 22:22:07 -0500

    Person 1 edit

--- src/main/java/edu/gatech/oad/antlab/person/Person1.java
+++ src/main/java/edu/gatech/oad/antlab/person/Person1.java
@@ -30,7 +30,7 @@ public class Person1 {
 	 * @return the modified string
 	 */
 	private String calc(String input) {
-	  if (input.length > 2) {
+	  if (input.length() > 2) {
 	  	String firstTwo = input.substring(0, 2);
 	  	return input.substring(2) + firstTwo;
 	  }
